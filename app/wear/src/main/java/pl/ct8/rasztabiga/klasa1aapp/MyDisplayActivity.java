a45c20ac48a61a709d9670a7fdec5c4b20783cf7
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2016-11-18 22:26:20 +0100

    Second initiall commit

---
+++ app/wear/src/main/java/pl/ct8/rasztabiga/klasa1aapp/MyDisplayActivity.java
@@ -0,0 +1,17 @@
+package pl.ct8.rasztabiga.klasa1aapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MyDisplayActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        mTextView = (TextView) findViewById(R.id.text);
+    }
+}
