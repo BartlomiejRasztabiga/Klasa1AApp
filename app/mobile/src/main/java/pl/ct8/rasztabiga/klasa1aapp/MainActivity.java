9fc1f003e81b1fe2551af8fd8851dc105c9ba616
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2016-11-22 20:21:46 +0100

    i hate this

--- app/mobile/src/main/java/pl/ct8/rasztabiga/klasa1aapp/MainActivity.java
+++ app/mobile/src/main/java/pl/ct8/rasztabiga/klasa1aapp/MainActivity.java
@@ -10,4 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //TODO https://spring.io/guides/gs/consuming-rest-android/
 }
