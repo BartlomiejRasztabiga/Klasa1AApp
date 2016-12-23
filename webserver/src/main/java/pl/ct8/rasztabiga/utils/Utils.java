af216f28929e1ee58344d949435ded88cc7d6408
Dawid <dawid.wasala1@gmail.com>
2016-12-23 16:20:44 +0100

    Addded Utils class

---
+++ webserver/src/main/java/pl/ct8/rasztabiga/utils/Utils.java
@@ -0,0 +1,15 @@
+package pl.ct8.rasztabiga.utils;
+
+/**
+ * Created by Dawid on 22.12.2016.
+ */
+public class Utils {
+
+    public static boolean convertIntToBoolean(int integer) {
+        if (integer == 1) {
+            return true;
+        }
+        return false;
+    }
+}
+
