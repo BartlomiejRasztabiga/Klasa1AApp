f85a15468de6b8393d980c98bee5a78e4e91275f
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-01-30 21:00:47 +0100

    getting apiKey is now working ;)

--- app/Klasa1A/app/src/main/java/pl/rasztabiga/klasa1a/utils/ApiKeyUtils.java
+++ app/Klasa1A/app/src/main/java/pl/rasztabiga/klasa1a/utils/ApiKeyUtils.java
@@ -13,9 +13,9 @@ public class ApiKeyUtils {
 
     public static String getApiKey(Context context) {
         //TODO Uncomment alter
-        //SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        //String apiKey = preferences.getString(context.getString(R.string.apiKey_pref_key), "");
-        //return apiKey;
-        return "tcknac";
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String apiKey = preferences.getString(context.getString(R.string.apiKey_pref_key), "");
+        return apiKey;
+        //return "tcknac";
     }
 }
